1634c0e89b26a0d99713eb83d5867a5f2f5ed5df
Rajatava Ghosh <ghoshrick10@gmail.com>
2025-12-19 01:36:55 +0530

    Added the socket informations and initialization

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -3,12 +3,14 @@
 #include <sstream>
 
 namespace{
+    
     void log(const std::string &message){ //& is used for the prohibition of any copy of the object
     std::cout<<message<<std::endl;
         }
+    
     void Exitwitherror(const std::string &errormessage){
-    std::cout<<WSAGetLastError<<std::endl;
-    log ("ERROR" + errormessage);
+    std::cout<<WSAGetLastError()<<std::endl;
+    log ("ERROR: " + errormessage);
     exit(1);
         } 
 }
@@ -16,14 +18,49 @@ namespace{
 namespace http
 {
     TcpServer::TcpServer(std::string IP_Adress,int port)
-    : m_ip_adress(IP_Adress),m_port(port),m_socket(),
-      m_new_socket(),m_incomingmessage(),m_socketAdress(),
-      m_socketadress_len(sizeof(m_socketAdress)),
-      m_serverMessage(buildResponse)
+    :   m_ip_adress(IP_Adress),
+        m_port(port),
+        m_socket(),
+        m_new_socket(),
+        m_incomingmessage(),
+        m_socketAdress(),
+        m_socketadress_len(sizeof(m_socketAdress)),
+        m_serverMessage(buildResponse)
 
     {
+        start_server();
     }
     TcpServer::~TcpServer()
+
     {
+        close_server();
     }
+    int TcpServer::start_server()
+    {
+        if (WSAStartup(MAKEWORD(2,0), &m_wsaData) != 0) //Loads the version of winsock version in 16 bits and we pass in the address for windows to fillup WSAdata giving access to the location
+        {
+            Exitwitherror("WSAstartup failed");
+        }
+        m_socket = socket(AF_INET,SOCK_STREAM,0); //Creating the socket
+        if (m_socket == INVALID_SOCKET){
+            Exitwitherror("Cannot create socket");
+            return 1;}
+        m_socketAdress.sin_family = AF_INET;
+        m_socketAdress.sin_port = htons(m_port);
+        m_socketAdress.sin_addr.s_addr = inet_addr(m_ip_address.c_str());
+        if (bind(m_socket,(sockaddr *)&m_socketAdress,m_socketadress_len) == SOCKET_ERROR)
+        {
+            Exitwitherror("Socket cannot be connected");
+            return 1;
+        }
+        return 0;
+    }
+
+    void TcpServer::close_server(){
+        closesocket(m_socket);
+        closesocket(m_new_socket);
+        WSACleanup();
+        exit(0);
+    }
+
 } // namespace http
--- src/http_server.h
+++ src/http_server.h
@@ -11,18 +11,22 @@ class TcpServer{
     TcpServer(std::string IP_Adress,int port);
     ~TcpServer();
     private:
-    std::string m_ip_adress;
+    std::string m_ip_address;
     int m_port;
-    SOCKET m_socket; //Over here in shit windows we use SOCKET cause it is considered a separate API i.e winsock 
-    SOCKET m_new_socket;
+    SOCKET m_socket; //Server socket,Over here in shit windows we use SOCKET cause it is considered a separate API i.e winsock 
+    SOCKET m_new_socket; //Client socket
     long m_incomingmessage;
-    struct sockaddr_in m_socketAdress;
+    struct sockaddr_in m_socketAdress; //As it is a C header we use struct(kind),sockaddr_in(type)
     int m_socketadress_len;
     std::string m_serverMessage;
+    std::string buildResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello from Servy!</h1></body></html>";
     WSADATA m_wsaData;
 
     int start_server();
     void close_server();
+    int listening();
+    int acceptConnections();
+
 };
 }
 #endif
